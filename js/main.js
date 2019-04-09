f435d1d9d96728976514421791f9136cbf88e887
vansy61 <vansy61@gmail.com>
2019-04-09 20:17:41 +0700

    abc

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(() =>{
+	$("header .row .text-logo h1").addClass("anime-zoom-in");
+	$("#main .container .row .image").addClass("anime-left-in");
+	$("#main .container .row .main-content").addClass("anime-right-in");
+})
