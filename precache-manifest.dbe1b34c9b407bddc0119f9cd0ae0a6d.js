e2e6240d8505ca323b309882e32e0f44847585ba
van sy <vansy61@gmail.com>
2020-08-30 17:43:06 +0700

    Updates

---
+++ precache-manifest.dbe1b34c9b407bddc0119f9cd0ae0a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e115bb969b07d8da1330c6b99a6f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44b777c8dd544deb8b5b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87ee4ac382e307e70e3d",
+    "url": "/static/js/2.ff0f0576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff0f0576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b777c8dd544deb8b5b",
+    "url": "/static/js/main.683a0073.chunk.js"
+  },
+  {
+    "revision": "fe2af3bd6869259f6b81",
+    "url": "/static/js/runtime-main.49925162.js"
+  }
+]);
